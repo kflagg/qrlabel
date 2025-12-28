32131e28e2fd4579cc5e99af7988f23f65842e76
Kenny Flagg <flagg.ka@gmail.com>
2025-12-28 13:15:05 -0700

    Deploy initial lambda (#2)
    Defined the CDK stack. Lambda code generates an SVG of the QR code.

--- cdk.py
+++ cdk.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import os
-import aws_cdk as cdk
+import aws_cdk
 
 from aws_cdk import (
     Stack,
+    Duration,
+    aws_lambda,
 )
 from constructs import Construct
 
@@ -13,10 +15,20 @@ class QrlabelStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # The lambda.
+        qrl_lambda = aws_lambda.Function(
+            self,
+            "KCTR",
+            code=aws_lambda.Code.from_asset_image(directory="."),
+            handler=aws_lambda.Handler.FROM_IMAGE,
+            runtime=aws_lambda.Runtime.FROM_IMAGE,
+            architecture=aws_lambda.Architecture.X86_64,
+            timeout=Duration.minutes(1),
+            memory_size=128,
+        )
 
 
-app = cdk.App()
-QrlabelStack(app, "QrlabelStack")
+app = aws_cdk.App()
+QrlabelStack(app, "QRLabelStack")
 
 app.synth()
