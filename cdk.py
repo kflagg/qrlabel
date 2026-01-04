18e57cefcde410ea29b60c3eced28197ad0e0938
Kenny Flagg <flagg.ka@gmail.com>
2026-01-04 13:50:05 -0700

    Deploy initial API gateway endpoints (#12)
    Initial API definitions.

--- cdk.py
+++ cdk.py
@@ -6,6 +6,7 @@ from aws_cdk import (
     Stack,
     Duration,
     aws_lambda,
+    aws_apigateway,
 )
 from constructs import Construct
 
@@ -18,13 +19,59 @@ class QrlabelStack(Stack):
         # The lambda.
         qrl_lambda = aws_lambda.Function(
             self,
-            "KCTR",
+            "QRL",
+            description="QR Label Generator",
             code=aws_lambda.Code.from_asset_image(directory="."),
             handler=aws_lambda.Handler.FROM_IMAGE,
             runtime=aws_lambda.Runtime.FROM_IMAGE,
             architecture=aws_lambda.Architecture.X86_64,
             timeout=Duration.minutes(1),
             memory_size=128,
+        )
+
+        # The QR label API.
+        qrlabel_api = aws_apigateway.LambdaRestApi(
+            self,
+            "QRLabel",
+            description="QR Image API",
+            handler=qrl_lambda,
+            proxy=False,
+            deploy=True,
+        )
+
+        # Root is the label resource.
+        qrlabel_api.root.add_method(
+            "GET",
+            request_parameters={
+                "method.request.querystring.text": True,
+                "method.request.querystring.upper": False,
+                "method.request.querystring.lower": False,
+                "method.request.querystring.error_correction": False,
+                "method.request.querystring.box_size": False,
+                "method.request.querystring.border": False,
+                "method.request.querystring.fill_color": False,
+                "method.request.querystring.back_color": False,
+                "method.request.querystring.encoding": False,
+                "method.request.querystring.method": False,
+                "method.request.querystring.xml_declaration": False,
+            },
+        )
+
+        # Add the image resource.
+        qrimage_api = qrlabel_api.root.add_resource("image")
+        qrimage_api.add_method(
+            "GET",
+            request_parameters={
+                "method.request.querystring.text": True,
+                "method.request.querystring.error_correction": False,
+                "method.request.querystring.box_size": False,
+                "method.request.querystring.border": False,
+                "method.request.querystring.fill_color": False,
+                "method.request.querystring.back_color": False,
+                "method.request.querystring.encoding": False,
+                "method.request.querystring.method": False,
+                "method.request.querystring.xml_declaration": False,
+            },
         )
 
 
